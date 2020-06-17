f10745eb9ec33105743b70772874d726cc5dd245
Brian Holt <bharrisonholt@gmail.com>
2020-06-17 16:10:07 -0700

    facebook auth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId:'1144778055876916',
+        clientSecret:'c5ce38d48e10289dd9f4b49529205309'
+    }
 }
